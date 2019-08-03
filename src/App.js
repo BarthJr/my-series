74b1360758cf4b5d4e3009a0422a9755e6c9209c
Barth <juniior.barth@gmail.com>
2019-08-03 17:01:26 -0300

    Now loads components properly

--- src/App.js
+++ src/App.js
@@ -1,35 +1,29 @@
-import React, {useState, useEffect} from 'react';
-import Header from "./Header";
-import NewGenre from "./NewGenre";
-import EditGenre from "./EditGenre";
-import Genres from "./Genres";
-import axios from 'axios'
+import React from 'react'
+import Header from './Header'
+import NewGenre from './NewGenre'
+import EditGenre from './EditGenre'
+import Genres from './Genres'
 import {
   BrowserRouter as Router,
-  Route
+  Route,
+  Switch
 } from 'react-router-dom'
-
 
 const Home = () => {
   return <h1>Home</h1>
 }
 
-function App() {
-  const [data, setData] = useState({})
-  useEffect(() => {
-    axios.get('/api').then(res => {
-      setData(res.data)
-    })
-  }, [])
+function App () {
   return (
     <Router>
       <div>
         <Header/>
-        <Route path='/' exact component={Home}/>
-        <Route path='/genres/:id' exact component={EditGenre}/>
-        <Route path='/genres/new' exact component={NewGenre}/>
-        <Route path='/genres' exact component={Genres}/>
-        <pre>{JSON.stringify(data)}</pre>
+        <Switch>
+          <Route path='/' exact component={Home}/>
+          <Route path='/genres' exact component={Genres}/>
+          <Route path='/genres/new' exact component={NewGenre}/>
+          <Route path='/genres/:id' exact component={EditGenre}/>
+        </Switch>
       </div>
     </Router>
   )
